58028932691e6abb1631161689582b924465f12b
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-07-02 20:13:09 +0300

    test2

--- moonfang-ver-1/vite.config.js
+++ moonfang-ver-1/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://topi1.github.io/moonfang-1/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/moonfang-1/'
+  }
+
+  return config
 })
